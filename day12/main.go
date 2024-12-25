53a3eace474098d41fcac63101e516d77e30860d
Magnus Andersson <mpja69@gmail.com>
2024-12-25 01:40:29 +0100

    solved p2, by scanning rows and cols

--- day12/main.go
+++ day12/main.go
@@ -25,6 +25,7 @@ func main() {
 	C = len(grid[0])
 
 	fmt.Printf("Part 1: (1930, 1550156): %d\n", part1(grid))
+	fmt.Printf("Part 2: (1206, 946084): %d\n", part2(grid))
 }
 
 type Pos struct{ r, c int }
@@ -104,6 +105,108 @@ func (g Grid) neighbours(p Pos) []Pos {
 		neighbours = append(neighbours, Pos{r, c})
 	}
 	return neighbours
+}
+
+// ---------------------------------------------- Part 2 ------------------------------------------------
+
+func part2(g Grid) int {
+	visited := make(map[Pos]bool)
+	price := 0
+	for r := range R {
+		for c := range C {
+			if visited[Pos{r, c}] {
+				continue
+			}
+			region, area, _ := g.findOneRegion(Pos{r, c})
+			maps.Insert(visited, maps.All(region))
+			id := g[r][c]
+			horizontalSides := g.scanRows(region, id)
+			verticalSides := g.scanCols(region, id)
+			perimeter := horizontalSides + verticalSides
+			price += area * perimeter
+		}
+	}
+	return price
+}
+
+func (g Grid) scanRows(region map[Pos]bool, id byte) int {
+	length := 0
+	// Scan each row...
+	for r := range R {
+		top := false
+		bottom := false
+		// ...from left to right
+		for c := range C {
+			if region[Pos{r, c}] == false {
+				top = false
+				bottom = false
+				continue
+			}
+			// Have one above
+			if r > 0 && g[r-1][c] == id {
+				top = false
+			}
+			// Have one below
+			if r < R-1 && g[r+1][c] == id {
+				bottom = false
+			}
+			// Have none above
+			if r == 0 || r > 0 && g[r-1][c] != id {
+				if top == false {
+					length++
+					top = true
+				}
+			}
+			// have none below
+			if r == R-1 || r < R-1 && g[r+1][c] != id {
+				if bottom == false {
+					length++
+					bottom = true
+				}
+			}
+		}
+	}
+	return length
+}
+
+func (g Grid) scanCols(region map[Pos]bool, id byte) int {
+	length := 0
+	// Scan each col...
+	for c := range C {
+		left := false
+		right := false
+		// ...from top to bottom
+		for r := range R {
+			if region[Pos{r, c}] == false {
+				left = false
+				right = false
+				continue
+			}
+			// have one left
+			if c > 0 && g[r][c-1] == id {
+				left = false
+			}
+			// have one right
+			if c < C-1 && g[r][c+1] == id {
+				right = false
+			}
+			// have none left
+			if c == 0 || c > 0 && g[r][c-1] != id {
+				if left == false {
+					length++
+					left = true
+				}
+			}
+			// have none right
+			if c == C-1 || c < C-1 && g[r][c+1] != id {
+				if right == false {
+					length++
+					right = true
+				}
+			}
+		}
+	}
+	return length
 }
 
 // ---------------------- Helper functions --------------------
